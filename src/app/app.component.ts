2ebcd17746d2aa46f96e959f4b0a69f8c82b8453
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-29 16:22:33 +0200

    Added custom validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Form, FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,12 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsers: ['Max', 'Maximilian'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -27,6 +28,16 @@ export class AppComponent implements OnInit {
 
   addHobbies() {
     const control = new FormControl(null, Validators.required);
+
+    // the array has to be explicitly casted to work
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+
+    if (this.forbiddenUsers.indexOf(control.value) !== -1) {
+      return {'nameForbidden': true};
+    }
+    return null;
   }
 }
