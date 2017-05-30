c567be884bf22d974e12785c65d52c592493fe32
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-05-30 16:15:09 +0200

    Prepopulating the form with dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,15 @@ export class AppComponent implements OnInit {
     this.signupForm.statusChanges.subscribe(
       (status) => console.log(status)
     );
+
+    this.signupForm.setValue({
+      'userData': {
+        'username': 'Primoz',
+        'email': 'primoz@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
   }
 
   onSubmit() {
